b7e96badbcd44e6f45b4705f0298bbe9e8301c87
restuak <restuaka.works@gmail.com>
2025-07-30 04:55:00 +0700

    add page dashboard

--- src/view/dashboard/page.tsx
+++ src/view/dashboard/page.tsx
@@ -1,9 +1,14 @@
-import Navbar from "@/app/dashboard/components/navbar/navbar";
+
+import SidebarMenuDashboard from "@/app/dashboard/components/sidebar/sidebar";
+import ListEvent from "@/app/dashboard/components/view/listofevent";
+import Navbar from "@/components/navbar/navbar";
 
 export default function DashboardView() {
   return (
     <>
       <Navbar />
+      <SidebarMenuDashboard />
+      <ListEvent />
     </>
   );
 }
