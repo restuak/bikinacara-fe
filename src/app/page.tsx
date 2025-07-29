59e32a95c85bab9cc96e7d48b57e3940bcdeaa46
jupiterveil <your-email@example.com>
2025-07-29 22:58:07 +0700

    Add Browse by Categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/navbar";
 import HomeView from "@/view/home/page";
 import PopularEvents from "@/components/popularevents";
+import BrowseCategories from "@/components/browsecategories";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <PopularEvents />
+      <BrowseCategories />
       <HomeView />
     </>
   );
